107caa419f25ad54f7d40c3e2b5d1be53790832f
Kasualix <670067575@qq.com>
2023-09-15 23:01:43 +0800

    complete

--- src/main/java/com/teampotato/isdpe/EventHandler.java
+++ src/main/java/com/teampotato/isdpe/EventHandler.java
@@ -1,7 +1,10 @@
 package com.teampotato.isdpe;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
@@ -16,40 +19,79 @@ import org.jetbrains.annotations.NotNull;
 import static com.teampotato.isdpe.InitialSpawnDimensionPotatoEdition.*;
 
 public class EventHandler {
-    @SubscribeEvent
-    public static void onPlayerLoggedIn(PlayerEvent.@NotNull PlayerLoggedInEvent event) {
-        if (spawnDimension.get().equals("minecraft:overworld")) return;
-        BlockPos.Mutable mutableBlockPos = new BlockPos.Mutable(spawnX.get(), spawnY.get(), spawnZ.get());
-        PlayerEntity player = event.getPlayer();
-        World world = player.level;
-        if (world instanceof ServerWorld) {
-            MinecraftServer server = ((ServerWorld)world).getServer();
-            ServerWorld destination = server.getLevel(RegistryKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(spawnDimension.get())));
-            if (destination == null) return;
-            server.getCommands().performCommand(server.createCommandSourceStack().withSuppressedOutput(), "/execute in " + spawnDimension.get() + " run tp " + player.getStringUUID() + " " + spawnX.get().floatValue() + " " + spawnY.get().floatValue() + " " + spawnZ.get().floatValue());
-            if (allowSpawnUnderground.get()) {
-                for (int i = spawnY.get(); i < 256; i ++) {
+    private static BlockPos.Mutable mutableBlockPos = null;
+
+    private static void initSpawn(@NotNull MinecraftServer server, CommandSource commandSource, Commands commands, PlayerEntity player, boolean safe) {
+        float x = spawnX.get().floatValue();
+        float y = spawnY.get().floatValue();
+        float z = spawnZ.get().floatValue();
+        String uuid = player.getStringUUID();
+        String spawnDim = spawnDimension.get();
+        ServerWorld destination = server.getLevel(RegistryKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(spawnDim)));
+        if (destination == null) return;
+        commands.performCommand(commandSource, "/execute in " + spawnDim + " run tp " + uuid + " " + x + " " + (y + 1.0F) + " " + z);
+        if (allowSpawnUnderground.get()) {
+            for (Integer i = spawnY.get(); i < destination.getMaxBuildHeight(); i ++) {
+                if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR) && !destination.getBlockState(mutableBlockPos.below()).is(Blocks.AIR)) {
+                    mutableBlockPos.setY(mutableBlockPos.getY() + 1);
                     if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR)) {
-                        mutableBlockPos.setY(mutableBlockPos.getY() + 1);
-                        if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR)) {
-                            player.setPos(player.getX(), i, player.getZ());
-                            break;
-                        }
-                        mutableBlockPos.setY(mutableBlockPos.getY() - 1);
+                        commands.performCommand(commandSource, "/execute in " + spawnDim + " run tp " + uuid + " " + x + " " + (i.floatValue() + 1.0F) + " " + z);
+                        safe = true;
+                        player.addTag("isdpe.spawned");
+                        break;
                     }
-                }
-            } else {
-                for (int i = 0; i < 256; i ++) {
-                    if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR) && destination.canSeeSky(mutableBlockPos)) {
-                        mutableBlockPos.setY(mutableBlockPos.getY() + 1);
-                        if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR)) {
-                            player.setPos(player.getX(), i, player.getZ());
-                            break;
-                        }
-                        mutableBlockPos.setY(mutableBlockPos.getY() - 1);
-                    }
+                    mutableBlockPos.setY(mutableBlockPos.getY() - 1);
                 }
             }
+        } else {
+            for (Integer i = spawnY.get(); i < destination.getMaxBuildHeight(); i ++) {
+                if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR) && destination.canSeeSky(mutableBlockPos) && !destination.getBlockState(mutableBlockPos.below()).is(Blocks.AIR)) {
+                    mutableBlockPos.setY(mutableBlockPos.getY() + 1);
+                    if (destination.getBlockState(mutableBlockPos).is(Blocks.AIR)) {
+                        commands.performCommand(commandSource, "/execute in " + spawnDim + " run tp " + uuid + " " + x + " " + (i.floatValue() + 1.0F) + " " + z);
+                        safe = true;
+                        player.addTag("isdpe.spawned");
+                        break;
+                    }
+                    mutableBlockPos.setY(mutableBlockPos.getY() - 1);
+                }
+            }
+        }
+        if (!safe) {
+            player.addTag("isdpe.spawned");
+            mutableBlockPos.setY(62);
+            destination.setBlockAndUpdate(mutableBlockPos, Blocks.STONE.defaultBlockState());
+            mutableBlockPos.setY(spawnY.get());
+            commands.performCommand(commandSource, "/execute in " + spawnDim + " run tp " + uuid + " " + x + " 64 " + z);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLoggedIn(PlayerEvent.@NotNull PlayerLoggedInEvent event) {
+        if (mutableBlockPos == null) mutableBlockPos = new BlockPos.Mutable(spawnX.get(), spawnY.get(), spawnZ.get());
+        if (spawnDimension.get().equals("minecraft:overworld")) return;
+        boolean safe = false;
+        PlayerEntity player = event.getPlayer();
+        World world = player.level;
+        if (world instanceof ServerWorld && !player.getTags().contains("isdpe.spawned")) {
+            MinecraftServer server = ((ServerWorld)world).getServer();
+            CommandSource commandSource = server.createCommandSourceStack().withSuppressedOutput();
+            Commands commands = server.getCommands();
+            initSpawn(server, commandSource, commands, player, safe);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerRespawn(PlayerEvent.@NotNull PlayerRespawnEvent event) {
+        PlayerEntity player = event.getPlayer();
+        World world = player.level;
+        boolean safe = false;
+        if (world instanceof ServerWorld && player instanceof ServerPlayerEntity) {
+            if (((ServerPlayerEntity)player).getRespawnPosition() != null) return;
+            MinecraftServer server = ((ServerWorld)world).getServer();
+            CommandSource commandSource = server.createCommandSourceStack().withSuppressedOutput();
+            Commands commands = server.getCommands();
+            initSpawn(server, commandSource, commands, player, safe);
         }
     }
 }
--- src/main/java/com/teampotato/isdpe/InitialSpawnDimensionPotatoEdition.java
+++ src/main/java/com/teampotato/isdpe/InitialSpawnDimensionPotatoEdition.java
@@ -22,10 +22,10 @@ public class InitialSpawnDimensionPotatoEdition {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.push("InitialSpawnDimensionPotatoEdition");
         spawnDimension = builder.define("spawnDimension", "minecraft:overworld");
-        allowSpawnUnderground = builder.define("allowSpawnUnderground", false);
+        allowSpawnUnderground = builder.comment("If the dimension you set has ceiling (Are you sure? This will be unsafe for players without pickaxes on spawn!), you should enable this").define("allowSpawnUnderground", false);
         spawnX = builder.defineInRange("spawnX", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
         spawnY = builder
-                .comment("This value will be adjusted based on the situation.", "You wouldn't want players to spawn inside blocks, would you?")
+                .comment("This value will be adjusted based on the situation.", "You wouldn't want players to spawn inside blocks, would you?", "If you want the player to spawn on normal ground, keep this at 0.")
                 .defineInRange("spawnY", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
         spawnZ = builder.defineInRange("spawnZ", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
         builder.pop();
